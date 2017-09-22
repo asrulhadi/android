93881284d667e73a5a0db79e833e889117e91932
Asrul Hadi <git@asrulhadi.net>
2017-09-22 19:01:19 +0800

    dynamic locale based on string

--- app/src/main/java/net/asrulhadi/localesetting/MainActivity.java
+++ app/src/main/java/net/asrulhadi/localesetting/MainActivity.java
@@ -1,8 +1,11 @@
 package net.asrulhadi.localesetting;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.Locale;
 
@@ -13,8 +16,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // specifically set the locale
         Configuration config = getResources().getConfiguration();
-        config.setLocale(new Locale("ms"));
+        config.setLocale(new Locale("jp"));
         // set the view
         setContentView(R.layout.activity_main);
+    }
+
+    public void tukarBahasa(View v) {
+        TextView h = (TextView) findViewById(R.id.hello_world);
+        Configuration cfg = getResources().getConfiguration();
+        cfg.setLocale(new Locale("ms"));
+        Context ctx = createConfigurationContext(cfg);
+        String hw = ctx.getResources().getString(R.string.hello);
+        h.setText(R.string.hello);
     }
 }
