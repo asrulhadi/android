b448f3c0e1dd3036754527a1b4678a3d313bf28f
Asrul Hadi <git@asrulhadi.net>
2017-09-25 17:14:10 +0800

    updating icons

--- app/src/main/java/net/asrulhadi/localesetting/MainActivity.java
+++ app/src/main/java/net/asrulhadi/localesetting/MainActivity.java
@@ -1,6 +1,7 @@
 package net.asrulhadi.localesetting;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,6 +28,20 @@ public class MainActivity extends AppCompatActivity {
         cfg.setLocale(new Locale("ms"));
         Context ctx = createConfigurationContext(cfg);
         String hw = ctx.getResources().getString(R.string.hello);
-        h.setText(R.string.hello);
+        // get all the array
+        String[] ha = getResources().getStringArray(R.array.hadis);
+        for ( int i=0 ; i<ha.length; i++ ) {
+            hw += "\n -> " + ha[i];
+        }
+        h.setText(hw);
+    }
+
+    public void shareText(View v) {
+        String thisText = (String) ((TextView) findViewById(R.id.hello_world)).getText();
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, thisText);
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.send_to)));
     }
 }
